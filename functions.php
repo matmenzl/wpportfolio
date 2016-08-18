9f422d8187594a7ffe461862e15c6f38f35def3f
matmenzl <info@mathiasmenzl.ch>
2016-08-19 01:20:01 +0200

    added basic navigation

--- functions.php
+++ functions.php
@@ -1,9 +1,22 @@
 <?php 
 
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+
+  register_nav_menus(
+    array(
+      'primary-menu'    => __( 'Primary Menu' )
+
+        )
+    );
+}
+
+add_action( 'init', 'register_theme_menus' );
 
 function wpt_theme_styles() {
   wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
-  wp_enqueue_style( 'googlefont-css', get_template_directory_uri() . '/http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+  wp_enqueue_style( 'googlefont-css', get_template_directory_uri() . 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
@@ -11,8 +24,8 @@ add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 function wpt_theme_js() {
 
   wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );
-  wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation/foundation.js', array('jquery'), '', true );
-  wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
+  wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
+  wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
