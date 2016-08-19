712d6fda72fd6bd794e7f4b5ac1f931c22c0e294
matmenzl <info@mathiasmenzl.ch>
2016-08-19 17:44:25 +0200

    added navigation class

--- header.php
+++ header.php
@@ -7,14 +7,26 @@
     <?php wp_head(); ?>
   </head>
 
-  <body>
+  <body <?php body_class(); ?>>
     <header class="row no-max pad main">
   <h1><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
   <a href="" class="nav-toggle"><span></span>Menu</a>
   <nav>
     <h1 class="open"><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-    <ul class="no-bullet">
-      <li class="current parent"><a class='current' href="index.html">Portfolio</a>
+    <?php 
+
+    $defaults = array(
+        'container' => false,
+        'theme_location' =>'primary-menu',
+        'menu_class' => 'no-bullet'
+      );
+
+      wp_nav_menu ( $defaults );
+
+      ?>
+
+    <!-- <ul class="no-bullet">
+      <li class="current parent"><a class='current' href="portfolio.html">Portfolio</a>
         <ul class="sub-menu">
           <li><a href="item.html">Portfolio Item</a></li>
           <li><a href="item.html">Portfolio Item</a></li>
@@ -30,6 +42,6 @@
       </li>
       <li><a href="about.html">About</a></li>
       <li><a href="contact.html">Contact</a></li>
-    </ul>
+    </ul> -->
   </nav>
 </header>
