bade9e9baf73b9b4f4b73f5f2b6a3cbb4b8c00ea
matmenzl <info@mathiasmenzl.ch>
2016-08-20 18:07:47 +0200

    added portfolio custom post types

--- page-portfolio.php
+++ page-portfolio.php
@@ -1,8 +1,9 @@
 <?php 
 /*
-  Template Name: Portfolio Page
+  Template name: Portfolio Page
 */
   ?>
+
 <?php get_header(); ?>  
 
 <section class="row">
@@ -12,13 +13,31 @@
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       
         <h1><?php the_title(); ?></h1>
-        <p>test</p>
         <?php the_content(); ?>       
       
-      <?php endwhile; endif; ?>
+      <?php endwhile; endif; wp_reset_postdata();?>
 
     </div>
   </div>
+</section>
+
+<?php 
+
+$args = array(
+    'post_type' => 'portfolio'
+  );
+  $query = new WP_Query( $args );
+?>
+
+<section class="row no-max pad">
+
+  <?php if ( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+  
+  <div class="small-6 medium-4 large-3 columns grid-item">
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
+  </div>
+
+  <?php endwhile; endif; wp_reset_postdata(); ?>
 </section>
 
 
