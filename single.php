000632464dca2080dbfb666e26becc3ca4850318
matmenzl <info@mathiasmenzl.ch>
2016-08-27 17:40:40 +0200

    added single.php

--- single.php
+++ single.php
@@ -12,25 +12,25 @@
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
             <article class="post">
-              <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
               <ul class="post-meta no-bullet">
-              <li class="date"><?php the_time( 'F j, Y'); ?></li>
+              <li class="date">on <?php the_time( 'F j, Y'); ?></li>
                 <li class="author">
                     <span class="wpt-avatar small">
                       <?php echo get_avatar( get_the_author_meta('ID'), 50 ); ?>
                     </span>
-                    <?php the_author_posts_link(); ?>
-                    <li>in <?php the_category( ' ' ); ?></li>
+                    by <?php the_author_posts_link(); ?> in <?php the_category( ' ' ); ?>
                 </li>
               </ul>
-              <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
-              <?php if( the_post_thumbnail() ) : ?>
+              <?php if( get_the_post_thumbnail() ) : ?>
                 <div class="img-container">
                   <?php the_post_thumbnail('large'); ?>
                   <p>Photo by Gratt Spore</p>
                 </div>
+                <?php endif; ?>
 
-              <?php endif; ?>
+              <?php the_content(); ?>
+
             </article>    
 
           <?php endwhile; else : ?>
